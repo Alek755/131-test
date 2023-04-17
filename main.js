d5993fca01e109849ec5373916c95cbe35acce5d
Alek755 <80863873+Alek755@users.noreply.github.com>
2023-04-16 20:35:46 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+canvas_background = "dog_cat.jpg";
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+}
+
+function draw() {
+    image(canvas_background, 0, 0, 600, 500);
+
+}
